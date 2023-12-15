a4d6f56f527f2894c1a4b3d73c61b561ee3bdd7e
Swarna Jang Maden <limbujungmaden@gmail.com>
2023-12-15 14:17:51 +0545

    setup tailwind

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -21,13 +21,27 @@ export default function Counter() {
   console.log("rendered");
   return (
     <>
-      <h1>Counter: {count}</h1>
-      <h1>Value: {data}</h1>
-      <hr />
-      <button onClick={increment}>increment count</button>
-      <button onClick={decrement}>decrement</button>
-      <button onClick={incrementValue}>increment state value</button>
-      <button onClick={decrementValue}>decrement state value</button>
+      <h1 className="text-2xl border">Counter: {count}</h1>
+      <h1 className="text-5xl font-bold underline p-4 text-center">
+        Value: {data}
+      </h1>
+      <div className="flex justify-center">
+        <button className="border bg-gray-300 p-5 ml-5" onClick={increment}>
+          increment count
+        </button>
+        <button className="border bg-blue-300 p-5 ml-5" onClick={decrement}>
+          decrement count
+        </button>
+        <button
+          className="border bg-green-300 p-5 ml-5"
+          onClick={incrementValue}
+        >
+          increment state value
+        </button>
+        <button className="border bg-red-300 p-5 ml-5" onClick={decrementValue}>
+          decrement state value
+        </button>
+      </div>
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./main.css";
+import "./index.css"; // tailwind css
+// import "./main.css"; // vanilla css
 import App from "./App";
+import Counter from "./components/Counter";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Counter />
+    {/* <App /> */}
   </React.StrictMode>
 );
