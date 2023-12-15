5210a71b851c0134c02854c2b8b8f243c18002a3
Swarna Jang Maden <limbujungmaden@gmail.com>
2023-12-15 14:51:30 +0545

    add tailwind classes

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -25,20 +25,29 @@ export default function Counter() {
       <h1 className="text-5xl font-bold underline p-4 text-center">
         Value: {data}
       </h1>
-      <div className="flex justify-center">
-        <button className="border bg-gray-300 p-5 ml-5" onClick={increment}>
+      <div className="flex justify-center gap-4">
+        <button
+          className="border bg-gray-300 p-5 rounded-lg hover:text-blue-600"
+          onClick={increment}
+        >
           increment count
         </button>
-        <button className="border bg-blue-300 p-5 ml-5" onClick={decrement}>
+        <button
+          className="border bg-blue-300 p-5 rounded-lg hover:text-blue-600"
+          onClick={decrement}
+        >
           decrement count
         </button>
         <button
-          className="border bg-green-300 p-5 ml-5"
+          className="border bg-green-300 p-5 rounded-lg hover:shadow-xl hover:bg-green-400"
           onClick={incrementValue}
         >
           increment state value
         </button>
-        <button className="border bg-red-300 p-5 ml-5" onClick={decrementValue}>
+        <button
+          className="border bg-red-300 p-5 rounded-lg hover:shadow-2xl hover:bg-red-400"
+          onClick={decrementValue}
+        >
           decrement state value
         </button>
       </div>
--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,11 @@ import "./index.css"; // tailwind css
 // import "./main.css"; // vanilla css
 import App from "./App";
 import Counter from "./components/Counter";
+import GoogleTabs from "./GoogleTabs";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <GoogleTabs />
     <Counter />
     {/* <App /> */}
   </React.StrictMode>
